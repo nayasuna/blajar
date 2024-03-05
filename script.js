d922ea683cd9097bff3dc3d8fc880326ab35b52d
nayasuna <nayasuna01@students.amikom.ac.id>
2024-03-06 01:47:37 +0700

    coba-coba

---
+++ script.js
@@ -0,0 +1,42 @@
+console.log("ini dari external js")
+
+// tipe data javascript
+
+// string
+"hallo saya naya"
+"naya"
+"20"
+
+// number
+10
+9
+8
+
+// boolean
+true
+false
+1 == 1
+!true
+20 < 100
+true && true
+
+// array
+[1,2,3,4]
+["kucing", "kambing"]
+
+// object
+    let kucing = {
+        nama: "tom",
+        warna: "biru"
+    }
+
+// variabel
+//let
+//const
+//var
+
+//let nama = ini adlaah sebuah variabel.. kalau variabel bisa diubah pake let kalau tidak const
+
+let nama = "alpha"
+
+
